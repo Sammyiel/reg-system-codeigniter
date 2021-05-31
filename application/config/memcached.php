d3e8aff04ed6247bf3673b670c621fdc4646aa93
Sammyiel <s.maisiba@alustudent.com>
2021-05-31 15:47:11 +0200

    2nd

---
+++ application/config/memcached.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config = array(
+	'default' => array(
+		'hostname' => '127.0.0.1',
+		'port'     => '443',
+		'weight'   => '1',
+	),
+);
